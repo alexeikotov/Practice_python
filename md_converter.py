30a94edfe469e4bc8db0a22c69cd9bfb10fd5723
alexeikotov <kotov_alexei@mail.ru>
2022-10-12 13:36:10 +0300

    Add files via upload

--- md_converter.py
+++ md_converter.py
@@ -1,10 +1,59 @@
-file = open('solution.py', 'r')
+INPUT_CODE_DELIMITER = '# ---end----'
 
-a=''
-for line in file:
-	a += line
- 
-a = a.split('# ---end----')
-print(a)
-ouf = open("out.txt", 'w')
-ouf.write(a[-1])
+def read_data(file_name):
+    file = open(file_name)
+    content = file.read()
+    file.close()
+    return content
+
+
+def write_data(file_name, data):
+    file = open(file_name, 'w')
+    content = "\n".join(data)   
+    file.write(content)
+    final_file.close()    
+
+
+def prepare_md_titles(data):
+    title = description = None
+
+    for line in data.split('\n'):
+        if line.startswith('# title'):
+            title = line.replace('# title ', '')
+        elif line.startswith('# description'):
+            description = line.replace('# description ', '')
+    
+    return title, description        
+    
+
+def prepare_md_format(title, description, source_code):
+    md_link = '-'.join(title.lower().split())
+    
+    template = """+ [{}}](#{})
+    
+    ## {}
+    
+    {}
+    
+    ```python
+    {}
+    ```""".format(title, md_link)
+    return ['+ [{}](#{})\n\n## {}\n\n'.format(, , title),
+                  description, '\n```python', content[1].split('\n', 1)[1], '\n```']
+    
+
+def convert_data(data):
+    titles, source_code = data.split(INPUT_CODE_DELIMITER)  
+    title, description = prepare_md_titles(titles)
+    format = prepare_md_format(title, description, source_code)  
+    return format
+
+
+def main():
+    content = read_data('diagonal_sum.py')
+    format = convert_data(content)
+    write_data('matrix.md', format)
+
+
+if __name__ == "__main__":
+    main()
